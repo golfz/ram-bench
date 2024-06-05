a404bf0fb9b5c5224e7d06f33db9bdaf419433a3
surattikorn <surattikorn@gmail.com>
2024-06-05 16:42:02 +0700

    refactor: extract func

--- main.go
+++ main.go
@@ -7,52 +7,57 @@ import (
 )
 
 const (
+	loops    = 5
 	dataSize = 1024 * 1024 * 1024 * 1 // 1 GB
-	loops    = 3
 )
+
+func printBenchmark(name string, data []byte, fn func([]byte)) time.Duration {
+	start := time.Now()
+
+	fn(data)
+
+	duration := time.Since(start)
+	fmt.Printf("%s Time taken to operate on %d GB of RAM: %v\n", name, dataSize/(1024*1024*1024), duration)
+
+	return duration
+}
+
+func writeBenchmark(data []byte) {
+	for i := range data {
+		data[i] = byte(rand.Intn(256))
+	}
+}
+
+func readBenchmark(data []byte, origin []byte) {
+	for i := range data {
+		data[i] = origin[i]
+	}
+}
+
+func copyBenchmark(data []byte, origin []byte) {
+	copy(data, origin)
+}
 
 func main() {
 	fmt.Println("Starting RAM benchmark...")
 
-	var totalWriteDuration, totalReadDuration, totalTransferDuration time.Duration
+	var totalWriteTime, totalReadTime, totalTransferTime time.Duration
 
 	for loop := 1; loop <= loops; loop++ {
 		fmt.Printf("\n--- Loop %d ---\n", loop)
 
-		// Allocate slices for benchmarking
 		writeData := make([]byte, dataSize)
 		readData := make([]byte, dataSize)
 		copyData := make([]byte, dataSize)
 
-		// Write benchmark
-		start := time.Now()
-		for i := range writeData {
-			writeData[i] = byte(rand.Intn(256))
-		}
-		writeDuration := time.Since(start)
-		fmt.Printf("Write Speed: Time taken to write %d GB of RAM: %v\n", dataSize/(1024*1024*1024), writeDuration)
-		totalWriteDuration += writeDuration
-
-		// Read benchmark
-		start = time.Now()
-		for i := range readData {
-			readData[i] = writeData[i]
-		}
-		readDuration := time.Since(start)
-		fmt.Printf("Read Speed: Time taken to read %d GB of RAM: %v\n", dataSize/(1024*1024*1024), readDuration)
-		totalReadDuration += readDuration
-
-		// Transfer benchmark
-		start = time.Now()
-		copy(copyData, writeData)
-		transferDuration := time.Since(start)
-		fmt.Printf("Transfer Rate: Time taken to copy %d GB of RAM: %v\n", dataSize/(1024*1024*1024), transferDuration)
-		totalTransferDuration += transferDuration
+		totalWriteTime += printBenchmark("Write Speed:", writeData, writeBenchmark)
+		totalReadTime += printBenchmark("Read Speed:", readData, func(data []byte) { readBenchmark(data, writeData) })
+		totalTransferTime += printBenchmark("Transfer Rate:", copyData, func(data []byte) { copyBenchmark(data, writeData) })
 	}
 
 	// Print summary and calculate averages
 	fmt.Println("\n--- Summary ---")
-	fmt.Printf("Average Write Speed: %v\n", totalWriteDuration/time.Duration(loops))
-	fmt.Printf("Average Read Speed: %v\n", totalReadDuration/time.Duration(loops))
-	fmt.Printf("Average Transfer Rate: %v\n", totalTransferDuration/time.Duration(loops))
+	fmt.Printf("Average Write Speed: %v\n", totalWriteTime/time.Duration(loops))
+	fmt.Printf("Average Read Speed: %v\n", totalReadTime/time.Duration(loops))
+	fmt.Printf("Average Transfer Rate: %v\n", totalTransferTime/time.Duration(loops))
 }
